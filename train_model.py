cd32f037e0bdd329d376e66be7fccbdd7d54b4d6
Karan Dalvi <karan.j.dalvi@gmail.com>
2017-06-04 18:22:46 -0400

    Classification using GradientBoostClassifier

---
+++ train_model.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import ensemble
+from sklearn.externals import joblib
+from sklearn.metrics import mean_absolute_error
+import numpy
+
+source = pd.read_csv("breastCancerWisconsin.csv")
+
+source = source[:].replace("?",numpy.NaN)
+source.dropna(inplace=True)
+
+Y = source["class"]
+
+del source["id"]
+del source["class"]
+X = source
+
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.3, random_state=0)
+
+gbc = ensemble.GradientBoostingClassifier(learning_rate=0.1, n_estimators=100, max_depth=3,
+                                 max_features=1, min_samples_leaf=2)
+
+gbc.fit(Xtrain, Ytrain)
+
+joblib.dump(gbc, "breastCancerTrainedModel.pkl")
+
+errorRate = mean_absolute_error(Ytrain, gbc.predict(Xtrain))
+print("Training Date:", errorRate)
+
+errorRate = mean_absolute_error(Ytest, gbc.predict(Xtest))
+print("Test Date:", errorRate)
+
+test = [Xtest.iloc[0]]
+print("Prediction: ", gbc.predict(test)[0])
+print("Actual:", Ytest.iloc[0])
